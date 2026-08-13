c99898ae5bdd0732202d82b928fb3d2405fbb34f
Kishan Kalathia <kishan.kalathia@technostacks.com>
2026-08-13 11:21:17 +0530

    Gemini Model Added along with Groq Model

--- modules/LLM/config/models.ts
+++ modules/LLM/config/models.ts
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
 import { ChatGroq } from "@langchain/groq";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 dotenv.config({ path: ".env.local" });
 
 export const groqModel = new ChatGroq({
   apiKey: process.env.GROQ_API_KEY,
-  model: process.env.LLM_MODEL as string,
+  model: process.env.GROQ_LLM_MODEL as string,
+  streaming: true,
+});
+
+export const geminiModel = new ChatGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_API_KEY,
+  model: process.env.GOOGLE_LLM_MODEL as string,
   streaming: true,
 });
